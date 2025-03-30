418fe744f0632d42c53a0a4c6f42850b1fc1d104
Siratee K <sirateek@gmail.com>
2025-03-30 17:54:15 +0700

    feat: Add LINE

--- flake.nix
+++ flake.nix
@@ -24,6 +24,7 @@
           environment.systemPackages = [
             pkgs.vim
             pkgs.nixfmt-rfc-style
+            pkgs.android-tools
           ];
 
           # Necessary for using flakes on this system.
@@ -42,26 +43,17 @@
           # $ darwin-rebuild changelog
           system.stateVersion = 6;
 
-          system.activationScripts.applications.text =
-            let
-              env = pkgs.buildEnv {
-                name = "system-applications";
-                paths = config.environment.systemPackages;
-                pathsToLink = "/Applications";
-              };
-            in
-            pkgs.lib.mkForce ''
-              # Set up applications.
-              echo "setting up /Applications..." >&2
-              rm -rf /Applications/Nix\ Apps
-              mkdir -p /Applications/Nix\ Apps
-              find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-              while read -r src; do
-                app_name=$(basename "$src")
-                echo "copying $src" >&2
-                ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
-              done
-            '';
+          # Homebrew
+          homebrew = {
+            enable = true;
+            casks = [
+              "warp"
+            ];
+            masApps = {
+              "LINE" = 539883307;
+            };
+            onActivation.cleanup = "zap";
+          };
 
           # The platform the configuration will be used on.
           nixpkgs.hostPlatform = "aarch64-darwin";
